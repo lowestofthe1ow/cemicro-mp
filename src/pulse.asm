f9a6f33e4b8d1028c395edb61eac6e2cb3fa50eb
Lorenz Bernard Marqueses <lorenz_marqueses@dlsu.edu.ph>
2025-12-01 12:53:18 +0800

    Loop pulse

--- src/pulse.asm
+++ src/pulse.asm
@@ -12,33 +12,28 @@ NUM_BYTES   EQU 25   ; 0x000 to 0x018
 COUNT       RMB 1    ; Loop counter for bytes
 
     ORG PRG_START
-            LDAA #5
+            LDAB #5
 
 ; ------------------------------------------------------------------------------
 ; Subroutine PULSE
-; Sends a 1ms pulse at bit 7 of PORTB
-; Pulse time: (2+4+6+5+(3+3)*PULSE_WIDTH+5)s / 2MHz
+; Sends an approx. n-ms pulse at bit 7 of PORTB
+; (6 + (3 + (3 + 3)*332) * n) / 2MHz ~= n*0.9975ms
 ;
 ; Parameters:
-;   ACCA: Duration of pulse in ms
+;   ACCB: Duration of pulse in ms
 ; Modifies: ACCA, ACCB, X
 ; ------------------------------------------------------------------------------
 
-; Calculate value of PULSE_WIDTH.
-; Will be 330+333*(n-1) for an n-ms pulse. Follows from equation above.
-PULSE       DECA
-            LDAB #333
-            MUL
-            ADDD #330
-
             LDAA #$80
-PULSE_LOOP  EORA #$80       ; 2 cycles
             STAA PORTB      ; 4 cycles
             BSR DELAY_1MS   ; 6 cycles
-            BRA PULSE_LOOP  ; 3 cycles
+            CLRB
+            RTS             ; 5 cycles
 
-DELAY_1MS   LDX PULSE_WIDTH ; 5 cycles
+DELAY_1MS   LDX #332        ; 3 cycles
 DELAY_LOOP  DEX             ; 3 cycles
             BNE DELAY_LOOP  ; 3 cycles
+            DECB            ; 2 cycles
+            BNE DELAY_1MS   ; 3 cycles
             RTS             ; 5 cycles
 
